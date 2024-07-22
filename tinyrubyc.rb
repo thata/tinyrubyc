8217f718f75731099d24b68d1685b933a997addb
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2024-07-22 15:12:59 +0900

    整数リテラルを評価を追加

---
+++ tinyrubyc.rb
@@ -0,0 +1,24 @@
+require 'minruby'
+
+# 受け取った構文木からアセンブリコードを生成する
+def gen(node)
+  if node[0] == "lit"
+    # 整数の場合
+    # 例 : node = ["lit", 123]
+    puts "  mov eax, #{node[1]}"
+  end
+end
+
+# 入力をパースする
+node = minruby_parse(gets)
+
+puts "  .intel_syntax noprefix"
+puts "  .globl main"
+puts "main:"
+puts "  push rbp"
+puts "  mov rbp, rsp"
+
+gen(node)
+
+puts "  pop rbp"
+puts "  ret"
