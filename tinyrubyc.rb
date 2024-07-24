eaaab702a46e77de8af112b7f82479285e9669b3
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2024-07-24 09:05:40 +0900

    四則演算を追加

--- tinyrubyc.rb
+++ tinyrubyc.rb
@@ -6,6 +6,42 @@ def gen(node)
     # 整数の場合
     # 例 : node = ["lit", 123]
     puts "  mov rax, #{node[1]}"
+  elsif %w(+ - * /).include?(node[0])
+    # 四則演算の場合
+    # 例 : node = ["+", ["lit", 1], ["lit", 2]]
+
+    # r12 と r13 の値をスタックへ退避
+    puts "  push r12"
+    puts "  push r13"
+
+    # 左辺の計算結果を r12 へ格納
+    gen(node[1])
+    puts "  mov r12, rax"
+
+    # 右辺の計算結果を r13 へ格納
+    gen(node[2])
+    puts "  mov r13, rax"
+
+    # 演算結果を rax へ格納
+    case node[0]
+    when "+"
+      puts "  add r12, r13"
+      puts "  mov rax, r12"
+    when "-"
+      puts "  sub r12, r13"
+      puts "  mov rax, r12"
+    when "*"
+      puts "  imul r12, r13"
+      puts "  mov rax, r12"
+    when "/"
+      puts "  mov rax, r12"
+      puts "  cqo"
+      puts "  idiv r13"
+    end
+
+    # r12 と r13 の値をスタックから復元
+    puts "  pop r13"
+    puts "  pop r12"
   end
 end
 
