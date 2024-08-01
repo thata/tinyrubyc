550bf7799d3d9571cca794bbfafd225afbd11d6c
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2024-08-01 20:40:02 +0900

    case のテストを追加

--- test.sh
+++ test.sh
@@ -25,9 +25,9 @@ assert() {
 # # func_call
 # assert 30 'p add(10, 20)'
 
-# # case
-# assert 2 'case 42; when 0; p(0); when 1; p(1); else p(2); end'
-# assert 1 'case 42; when 0; p(0); when 42; p(1); else p(2); end'
+# case
+assert 2 'case 42; when 0; p(0); when 1; p(1); else p(2); end'
+assert 1 'case 42; when 0; p(0); when 42; p(1); else p(2); end'
 
 # while
 assert 55 'i = 1; sum = 0; while i <= 10; sum = sum + i; i = i + 1; end; p(sum)'
